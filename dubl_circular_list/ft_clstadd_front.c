bdfd34c656cbcfa3b86771f5590f599427569ba3
Fritz Gabler <fgabler@2-C-3.42heilbronn.de>
2023-09-03 16:18:37 +0200

    correction of ft_clstadd_front.c

--- dubl_circular_list/ft_clstadd_front.c
+++ dubl_circular_list/ft_clstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: fgabler <mail@student.42heilbronn.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 12:28:53 by fgabler           #+#    #+#             */
-/*   Updated: 2023/08/30 16:56:08 by fgabler          ###   ########.fr       */
+/*   Updated: 2023/09/03 16:18:12 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,5 @@ void	ft_clstadd_front(t_dubl_list **lst, t_dubl_list *new)
 	new->prev = tail;
 	tail->next = new;
 	(*lst)->prev = new;
+	*lst = new;
 }
