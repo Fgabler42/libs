2f75818f7ea079bf8c3b8cd125f2b31b47b4d0af
Fritz Gabler <fgabler@1-C-1.42heilbronn.de>
2023-09-25 16:37:08 +0200

    added unigend strlen

--- string/ft_strlen_uns.c
+++ string/ft_strlen_uns.c
@@ -6,13 +6,13 @@
 /*   By: fgabler <fgabler@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 15:05:09 by fgabler           #+#    #+#             */
-/*   Updated: 2023/09/25 16:32:13 by fgabler          ###   ########.fr       */
+/*   Updated: 2023/09/25 16:35:43 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen_uns(const char *str)
+size_t	ft_strlen_uns(const unsigned char *str)
 {
 	size_t	i;
 
